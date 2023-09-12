321c63cb9c350b0238ef2cb775aaa2283ed75821
Tan Teck Jun <tanteckjun@gmail.com>
2023-09-12 16:49:04 +0800

    Added grid weight smoothing
    By performing blurring between grid node weights after creation

--- Assets/Scripts/NavigationSystem/NavigationGrid.cs
+++ Assets/Scripts/NavigationSystem/NavigationGrid.cs
@@ -173,6 +173,63 @@ namespace TankGame.NavigationSystem
                     grid[x, y] = new GridNode(walkable, nodeWorldPosition, x, y, movementPenalty);
                 }
             }
+
+            BlurPenaltyMap(3);
+        }
+
+        /// <summary>
+        /// Introduce gradient to terrain penalties to prevent movement on edge of roads
+        /// </summary>
+        /// <param name="blurSize"></param>
+        private void BlurPenaltyMap(int blurSize)
+        {
+            int kernelSize = blurSize * 2 + 1; // Must be odd
+            int kernelExtents = (kernelSize - 1) / 2; // Number of squares between centre to edge of kernel
+
+            int[,] penaltiesHorizontalPass = new int[gridSizeX, gridSizeY];
+            int[,] penaltiesVerticalPass = new int[gridSizeX, gridSizeY];
+
+            for (int y = 0; y < gridSizeY; y++) { 
+                for (int x = -kernelExtents; x <= kernelExtents; x++) 
+                {
+
+                    int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                    penaltiesHorizontalPass[0, y] += grid[sampleX, y].movementPenalty;
+                }
+
+                for (int x = 1; x < gridSizeX; x++)
+                {
+                    int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX);
+                    int addIndex = Mathf.Clamp(x + kernelExtents, 0, gridSizeX - 1);
+
+                    penaltiesHorizontalPass[x, y] = 
+                        penaltiesHorizontalPass[x - 1, y] - grid[removeIndex, y].movementPenalty + grid[addIndex, y].movementPenalty;
+                }
+            }
+            
+            for (int x = 0; x < gridSizeX; x++) { 
+                for (int y = -kernelExtents; y <= kernelExtents; y++) 
+                {
+
+                    int sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                    penaltiesVerticalPass[x, 0] += penaltiesHorizontalPass[x, sampleY];
+                }
+
+                int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, 0] / (kernelSize * kernelSize));
+                grid[x, 0].movementPenalty = blurredPenalty;
+
+                for (int y = 1; x < gridSizeY; y++)
+                {
+                    int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY);
+                    int addIndex = Mathf.Clamp(y + kernelExtents, 0, gridSizeY - 1);
+
+                    penaltiesVerticalPass[x, y] =
+                        penaltiesVerticalPass[x, y - 1] - penaltiesHorizontalPass[x, removeIndex] + penaltiesHorizontalPass[x, addIndex];
+
+                    blurredPenalty = Mathf.RoundToInt((float) penaltiesVerticalPass[x, y] / (kernelSize * kernelSize));
+                    grid[x, y].movementPenalty = blurredPenalty;
+                }
+            }
         }
     }
 }
